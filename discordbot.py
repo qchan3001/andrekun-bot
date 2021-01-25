6c829c73f3f5d66563053fe9af3b1e35c0f5c2a6
qchan3001 <77869473+qchan3001@users.noreply.github.com>
2021-01-26 08:11:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,9 +8,10 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 @bot.event
 async def on_message(message):
-    if message.content == "!ping":
-        await message.channel.send("pong!")
-
+    if client.user in message.mentions:
+        reply = f'{message.author.mention} 呼んだ？'
+        await message.channel.send(reply)
+        
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
